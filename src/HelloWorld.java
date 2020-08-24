225af84a28dc954e362f88cfef4597629afbe6b4
Pawlak1992 <przemyslaw.pawlukiewicz@gmail.com>
2020-08-24 22:48:01 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        System.out.println("Counting to 10");
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
